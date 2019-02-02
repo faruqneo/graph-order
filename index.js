fc9cdd02966f2a0691acbee05e5456b86139ec23
faruqneo <faruq@marmeto.com>
2019-02-02 14:25:49 +0530

    form page view

--- index.js
+++ index.js
@@ -18,12 +18,36 @@ const shopify = new Shopify({
 app.get('/',function(req, res){
     shopify.order.list()
     .then( (orders) => {
+        
         res.render('index',{
             title: "order list",
             order: orders
         })
     })
     .catch(err => console.log(err));
+});
+
+app.get('/data/view/:proid/:id',function(req, res){
+    var i; 
+    let proid = req.params.proid;
+   // console.log(proid)
+  shopify.order.get(req.params.id)
+  .then(function(order){
+    
+    for (i=0; i < order.line_items.length; i++)
+    {
+        if(order.line_items[i].id == proid)
+        {
+           // res.send(order.line_items[i])
+            res.render('product',{
+                title: "product details",
+                product: order.line_items[i]
+            })
+        }
+    }
+    
+  })
+  .catch(err => console.log(err))
 });
 
 app.listen(3000, function(req, res){
